6f5be15551c7fdecfdaa9a75dff9b448a61762e7
Runtime <runtime@Runtimes-MBP.home>
2022-11-30 16:27:01 +0000

    Home styling changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -34,33 +34,37 @@ export default function Home() {
     });
 
     return (
-        <h3>
-            <Form id="search-form" role="search">
-                <input
-                    id="q"
-                    aria-label="Search movies"
-                    placeholder="Search"
-                    type="search"
-                    name="q"
-                    defaultValue={q}
-                />
-            </Form>
-            {movies.length ? (
-                <ul className="movie-list">
-                    {movies.map(movie => (
-                        <li key={movie.id} className="movie">
-                            <Link to={`movies/${movie.id}`}>
-                                <img
-                                    src={"https://image.tmdb.org/t/p/original" + movie.poster_path}
-                                    className="movie-poster"
-                                    alt={movie.title}
-                                />
-                            </Link>
+        <div>
+            <div className='movie-search'>
+                <Form id="search-form" role="search">
+                    <input
+                        id="q"
+                        aria-label="Search movies"
+                        placeholder="Search"
+                        type="search"
+                        name="q"
+                        defaultValue={q}
+                    />
+                </Form>
+            </div>
+            <div>
+                {movies.length ? (
+                    <ul className="movie-list">
+                        {movies.map(movie => (
+                            <li key={movie.id} className="movie">
+                                <Link to={`movies/${movie.id}`}>
+                                    <img
+                                        src={"https://image.tmdb.org/t/p/original" + movie.poster_path}
+                                        className="movie-poster"
+                                        alt={movie.title}
+                                    />
+                                </Link>
 
-                        </li>
-                    ))}
-                </ul>
-            ) : <p><i>No movies</i></p>}
-        </h3>
+                            </li>
+                        ))}
+                    </ul>
+                ) : <p><i>No movies</i></p>}
+            </div>
+        </div>
     );
 }
