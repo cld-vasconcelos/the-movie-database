d18521fc0c49757cd060fae55e5d0406dfc50ba1
Runtime <runtime@Runtimes-MBP.home>
2022-11-30 15:49:10 +0000

    Fetch movie in Movie page

--- src/pages/Movie/Movie.jsx
+++ src/pages/Movie/Movie.jsx
@@ -1,5 +1,11 @@
 import { useLoaderData } from "react-router-dom";
 
+import './Movie.css';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 async function GetMovie(movieId) {
     return fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=4ac15274164d3710a133b4a3023705c6`, {
         method: "GET",
@@ -19,6 +25,38 @@ export default function Movie() {
     const movie = useLoaderData();
     console.log(movie)
     return (
-        <h3>{movie.title}</h3>
+        <>
+            <div className="movie-detail">
+                <div className="movie-detail-poster">
+                    <img
+                        src={"https://image.tmdb.org/t/p/original" + movie.poster_path}
+                        className="movie-poster"
+                        alt={movie.title}
+                    />
+                </div>
+                <div class="movie-detail-info">
+                    <div className="movie-detail-title">
+                        {movie.title}
+                    </div>
+                </div>
+            </div>
+            {/* <div className="movie-detail">
+                <div className="movie-detail-poster">
+                    <img
+                        src={"https://image.tmdb.org/t/p/original" + movie.poster_path}
+                        className="movie-poster"
+                        alt={movie.title}
+                    />
+                </div>
+                <div class="movie-detail-info">
+                <div className="movie-detail-title">
+                        {movie.title}
+                    </div>
+                    <div className="movie-detail-title">
+                        {movie.release_date.split("-")[0]} • {movie.runtime}m
+                    </div>
+                </div>
+            </div> */}
+        </>
     );
 }
