692e5464d26f5d92245a730a08f04487ae484e16
Runtime <runtime@Runtimes-MBP.home>
2022-12-02 16:45:56 +0000

    Home page refactorization

---
+++ src/components/TopPicks/TopPicks.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import "./TopPicks.css";
+
+export default function TopPick(props) {
+    const picks = props.picks;
+
+    return (
+        <div className="top-picks-wrapper">
+            <h3> Top {props.type ? `${props.type}s` : "Picks"} </h3>
+            <div className='top-picks'>
+                {picks.length ? (
+                    <ul>
+                        {picks.map(pick => (
+                            <li key={pick.id} className="top-pick">
+                                <Link to={`tv/${pick.id}`}>
+                                    <img
+                                        src={"https://image.tmdb.org/t/p/original" + pick.poster_path}
+                                        alt={pick.title}
+                                    />
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                ) : <p><i>No results</i></p>}
+            </div>
+        </div>
+    );
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 
-import { Link, useLoaderData } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
+import TopPick from '../../components/TopPicks/TopPicks.jsx';
 import { GetTopMovies } from '../../helpers/movies.js';
 import { GetTopShows } from '../../helpers/shows.js';
 import './Home.css';
@@ -14,54 +15,9 @@ export default function Home() {
     const { movies, shows } = useLoaderData();
 
     return (
-        <div>
-            
-            <div className="top-movies">
-                <h3 style={{ textAlign: "left" }}>
-                    Top movies
-                </h3>
-                <div className='top-movies-list'>
-                    {movies.length ? (
-                        <ul>
-                            {movies.map(movie => (
-                                <li key={movie.id} className="top-movie">
-                                    <Link to={`movies/${movie.id}`}>
-                                        <img
-                                            src={"https://image.tmdb.org/t/p/original" + movie.poster_path}
-                                            alt={movie.title}
-                                        />
-                                    </Link>
-
-                                </li>
-                            ))}
-                        </ul>
-                    ) : <p><i>No results</i></p>}
-                </div>
-            </div>
-
-
-            <div className="top-movies">
-                <h3 style={{ textAlign: "left" }}>
-                    Top TV Shows
-                </h3>
-                <div className='top-movies-list'>
-                    {shows.length ? (
-                        <ul>
-                            {shows.map(show => (
-                                <li key={show.id} className="top-movie">
-                                    <Link to={`tv/${show.id}`}>
-                                        <img
-                                            src={"https://image.tmdb.org/t/p/original" + show.poster_path}
-                                            alt={show.title}
-                                        />
-                                    </Link>
-
-                                </li>
-                            ))}
-                        </ul>
-                    ) : <p><i>No results</i></p>}
-                </div>
-            </div>
-        </div>
+        <>
+            <TopPick type="Movie" picks={movies} />
+            <TopPick type="TV Show" picks={shows} />
+        </>
     );
 }
