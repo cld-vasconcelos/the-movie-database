4cbf7122d229c305384168bc2571ec5ee492e3ad
Runtime <runtime@Runtimes-MBP.home>
2022-12-02 18:36:28 +0000

    Extended search to include tv shows

--- src/helpers/movies.js
+++ src/helpers/movies.js
@@ -1,20 +1,7 @@
 import config from "../config.js"
 
 export async function GetTopMovies() {
-    console.log();
     const url = `${config.api.baseUrl}/movie/popular?${config.api.apiKey}&language=en-US&page=1`;
-    return fetch(url, {
-        method: "GET",
-        header: {
-            "Content-Type": "application/json"
-        }
-    })
-        .then(response => response.json())
-        .then(response => response.results.slice(0, 5))
-}
-
-export async function GetMovies(q) {
-    const url = `${config.api.baseUrl}/search/movie?${config.api.apiKey}&language=en-US&query=${q}&page=1&include_adult=false`;
     return fetch(url, {
         method: "GET",
         header: {
--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,6 +1,6 @@
 import { useLoaderData, useNavigate } from "react-router-dom";
 import config from "../../config";
-import { GetMovies } from "../../helpers/movies";
+import { SearchMedia } from "../../helpers/search";
 
 import "./Search.css";
 
@@ -8,17 +8,17 @@ export async function loader({ request }) {
     const url = new URL(request.url);
     const q = url.searchParams.get("q");
 
-    const movies = await GetMovies(q);
+    const results = await SearchMedia(q);
 
-    return { q, movies };
+    return { q, results };
 }
 
 export default function Search() {
-    const { q, movies } = useLoaderData();
+    const { q, results } = useLoaderData();
     const navigate = useNavigate();
 
-    const redirectTo = (movieId) => {
-        navigate(`/movies/${movieId}`)
+    const redirectTo = (movie) => {
+        navigate(`/${movie.media_type}/${movie.id}`)
     }
 
     return (
@@ -27,21 +27,21 @@ export default function Search() {
                 <h3>Search '{q}'</h3>
                 <div className="search-results">
                     <ul>
-                        {movies.map((movie) => (
-                            <li onClick={() => { redirectTo(movie.id) }}>
+                        {results.map((movie) => (
+                            <li onClick={() => { redirectTo(movie) }}>
                                 <div className="search-result-poster">
                                     <img
                                         src={`${config.imageBaseUrl}${movie.poster_path}`}
-                                        alt={movie.title}
+                                        alt={movie.media_type === "tv" ? movie.name : movie.title}
                                     />
                                 </div>
                                 <div class="search-result-info-wrapper">
                                     <div class="search-result-info vertically-centered">
                                         <span className="search-result-name">
-                                            {movie.title}
+                                            {movie.media_type === "tv" ? movie.name : movie.title}
                                         </span>
                                         <span className="search-result-year">
-                                            {movie.release_date.split("-")[0]}
+                                            {(movie.media_type === "tv" ? movie.first_air_date : movie.release_date).split("-")[0]}
                                         </span>
                                     </div>
                                 </div>
